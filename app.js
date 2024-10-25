63608289445817553c3a5007ab09e264d5aad317
karaus1977 <karausmiller@yahoo.com>
2024-10-25 00:25:48 +0000

    Kelsi's source code download 1st day 10/24/2024

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
-
+//this is a test
 menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
